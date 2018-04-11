a760a3fd6182d99419202c23c2b4f6494eb70205
fraxd <fraxd98@gmail.com>
2018-04-11 16:56:16 -0300

    v0.0.5
    Login Visualmente listo

---
+++ src/clases/Inicio.java
@@ -0,0 +1,14 @@
+package clases;
+
+import ventana.Login;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		Login login = new Login();
+		login.setVisible(true);
+		
+
+	}
+
+}
--- src/ventana/Login.java
+++ src/ventana/Login.java
@@ -9,10 +9,15 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField userField;
+	private JPasswordField passwordField;
 
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,8 +29,34 @@ public class Login extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblUser = new JLabel("Usuario:");
-		lblUser.setFont(new Font("Calibri", Font.BOLD, 20));
-		lblUser.setBounds(10, 72, 85, 29);
+		lblUser.setFont(new Font("Roboto", Font.BOLD, 20));
+		lblUser.setBounds(25, 115, 85, 29);
 		contentPane.add(lblUser);
+		
+		userField = new JTextField();
+		userField.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		userField.setBounds(144, 115, 216, 29);
+		contentPane.add(userField);
+		userField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Contrase\u00F1a:");
+		lblNewLabel.setFont(new Font("Roboto", Font.BOLD, 20));
+		lblNewLabel.setBounds(21, 176, 140, 29);
+		contentPane.add(lblNewLabel);
+		
+		passwordField = new JPasswordField();
+		passwordField.setToolTipText("Contrase\u00F1a");
+		passwordField.setBounds(144, 176, 216, 29);
+		contentPane.add(passwordField);
+		
+		JButton btnLogin = new JButton("Ingresar");
+		btnLogin.setFont(new Font("Roboto", Font.BOLD, 20));
+		btnLogin.setBounds(144, 227, 132, 23);
+		contentPane.add(btnLogin);
+		
+		JLabel lblNewLabel_1 = new JLabel("CubiSentiBuster");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 40));
+		lblNewLabel_1.setBounds(46, 11, 334, 78);
+		contentPane.add(lblNewLabel_1);
 	}
 }
