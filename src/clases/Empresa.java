14a40905bb7dc50098d1c4ad3a795c875d3fc89c
pelonchaz <kami-sama_no_kuso@live.cl>
2018-04-11 18:47:29 -0300

    V0.0.8
    Agregada la class Empresa, con ArrayList<Usuario> y metodos para agregar usuario y verificar usuario

---
+++ src/clases/Empresa.java
@@ -0,0 +1,31 @@
+package clases;
+
+import java.util.ArrayList;
+
+public class Empresa {
+	private ArrayList<Usuario> Usuarios;
+	
+	public Empresa() {
+		Usuarios= new ArrayList<Usuario>();
+	}
+	public boolean agregarUsuario(Usuario n) { //retorna false si el id ya existe, sino, agrega el usuario y retorna true;
+		int i;
+		for(i=0;i<(Usuarios.size());i++) {
+			if(Usuarios.get(i).getId()==n.getId())return false;
+		}
+		Usuarios.add(n);
+		return true;
+	}
+	public int verificarUsuario(int id, String password) { //1 es usuario correcto, 2 el usuario no existe, y 3 contraseña incorrecta
+		int i;
+		for(i=0;i<(Usuarios.size());i++) {
+			if(id==Usuarios.get(i).getId()) {
+				if(password==Usuarios.get(i).getPass()) {
+					return 1;
+				}
+				return 3;
+			}
+		}
+		return 2;
+	}
+}
