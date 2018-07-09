28c31cc3dffee79a71b933710d334f38e1091877
pelonchaz <kami-sama_no_kuso@live.cl>
2018-07-08 22:46:44 -0400

    v0.0.57
    Creada clase Rate, reemplazara el actual rate en el resto de las clases

---
+++ src/clases/Rate.java
@@ -0,0 +1,36 @@
+package clases;
+
+public class Rate {
+	long votos; ///cantidad total de votos
+	long rate; ///rate entre todas las votaciones
+	
+	///////////Constructor///////
+	public Rate() {
+		votos=0;
+		rate=0;
+	}
+	////////////////////
+	///Zona Getters y Setters///
+	public long getVotos() {
+		return votos;
+	}
+
+	public void setVotos(long votos) {
+		this.votos = votos;
+	}
+
+	public long getRate() {
+		return rate;
+	}
+
+	public void setRate(long rate) {
+		this.rate = rate;
+	}
+	/////////////////////////////
+	/////////////////////////////
+
+	void finalRate(long voto) {///voto es el rate que le dio el usuario, se promedia inmediatamente con los votos del resto
+		votos++;
+		rate=((rate*(votos-1))/votos)+(voto/votos);
+	}
+}
